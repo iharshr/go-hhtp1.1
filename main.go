61222fa01da3c08a2f6e69f170201304aa1ddeb9
Harsh Rawat <harsh@example.com>
2025-08-16 18:37:20 +0530

    feat: read file till \n , with 8 bytes chunks

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,13 +15,39 @@ func main() {
 	}
 	defer file.Close()
 	buffer := make([]byte, 8)
+	var parts string
 
 	for {
-		chunk, err := file.Read(buffer)
-		if err != nil {
+		f, err := file.Read(buffer)
+		// handeling end of file here
+		if err == io.EOF {
+			if parts != "" {
+				fmt.Printf("read: %s\n", string(buffer[:f]))
+			}
 			break
 		}
-		fmt.Printf("read: %s\n", string(buffer[:chunk]))
+		// store the 8 bites here
+		chunk := string(buffer[:f])
+		// add to line buffer
+		// this line buffer is of length till \n
+		parts += chunk
+
+		for {
+			new_line_index := strings.Index(parts, "\n")
+			if new_line_index == -1 {
+				// this means no new line
+				// we are still reading
+				// so break from this loop
+				break
+			}
+
+			current_line := parts[:new_line_index]
+			fmt.Printf("read: %s\n", string(current_line))
+
+			// clear buffer for new line
+			// so we can read next line
+			parts = parts[new_line_index+1:]
+		}
 
 	}
 }
